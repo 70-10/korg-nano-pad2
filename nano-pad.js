f4edd604e39eae787f9ee2349a27ffa7a2a8fd6c
70_10 <7010.dev@gmail.com>
2017-12-06 01:03:15 +0900

    [mod] button

--- nano-pad.js
+++ nano-pad.js
@@ -16,7 +16,7 @@ class NanoPad extends EventEmitter {
       const button = {
         state: getState(message[0]),
         type: getType(message[1]),
-        scene: getScene(message[1]),
+        scene: getScene(message[1])
       };
 
       this.emit("event", deltaTime, button);
@@ -29,18 +29,21 @@ class NanoPad extends EventEmitter {
   stop() {
     this.input.closePort();
   }
-
 }
 
 module.exports = NanoPad;
 
 function getState(value) {
+  if (value === 128) {
+    return "leave";
+  }
+
   if (value === 144) {
     return "press";
   }
 
-  if (value === 128) {
-    return "leave";
+  if (value === 176) {
+    return "move";
   }
 
   if (value === 240) {
@@ -50,35 +53,84 @@ function getState(value) {
 }
 
 function getType(value) {
-  if (value === 37 || value === 53 || value === 69 || value === 85) {
+  switch (value) {
+    case 1:
+      return "start";
+    case 2:
+      return "stop";
+    case 16:
+      return "leave";
+  }
+
+  if ((value - 20) % 16 === 0) {
+    return "C# / User";
+  }
+  if ((value - 21) % 16 === 0) {
     return "C / Chromatic";
   }
-
-  if (value === 39 || value === 55 || value === 71 || value === 87) {
+  if ((value - 22) % 16 === 0) {
+    return "A / minor1";
+  }
+  if ((value - 23) % 16 === 0) {
     return "C# / Major1";
   }
+  if ((value - 24) % 16 === 0) {
+    return "A# / minor2";
+  }
+  if ((value - 25) % 16 === 0) {
+    return "D / Major2";
+  }
+  if ((value - 26) % 16 === 0) {
+    return "B / m.Penta";
+  }
+  if ((value - 27) % 16 === 0) {
+    return "D# / M.Penta";
+  }
+  if ((value - 28) % 16 === 0) {
+    return "Range - / m.Blues";
+  }
+  if ((value - 29) % 16 === 0) {
+    return "E / M.Blues";
+  }
+  if ((value - 30) % 16 === 0) {
+    return "Range + / Raga";
+  }
+  if ((value - 31) % 16 === 0) {
+    return "F / Bass Line";
+  }
+  if ((value - 32) % 16 === 0) {
+    return "Oct - / Ryukyu";
+  }
+  if ((value - 33) % 16 === 0) {
+    return "F# / China";
+  }
+  if ((value - 34) % 16 === 0) {
+    return "Oct + / 5th";
+  }
+  if ((value - 35) % 16 === 0) {
+    return "G / 4th";
+  }
 
-  if(value === 66) {
+  if (value === 66) {
     return "scene";
   }
+
   return "none";
 }
 
 function getScene(value) {
-  if (value === 37 || value === 39) {
-    return "1";
+  if (value >= 36 && value <= 51) {
+    return 1;
+  }
+  if (value >= 52 && value <= 67) {
+    return 2;
+  }
+  if (value >= 68 && value <= 83) {
+    return 3;
+  }
+  if (value >= 84 && value <= 99) {
+    return 4;
   }
 
-  if (value === 53 || value === 55) {
-    return "2";
-  }
-
-  if (value === 69 || value === 71) {
-    return "3";
-  }
-
-  if (value === 85 || value === 87) {
-    return "4";
-  }
-  return "none";
+  return 0;
 }
